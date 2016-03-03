a41821b6acfe9f72b02903a18b0ca6f6ec8f906d
José L. Junior Garza <correojunior@gmail.com>
2016-03-03 07:03:10 +0000

    Step 0 - Bootstrapping

---
+++ client/app.ts
@@ -0,0 +1,14 @@
+import {Component, View} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+    selector: 'app'
+})
+
+@View({
+    templateUrl: 'client/app.html'
+})
+
+class Socially {}
+
+bootstrap(Socially);
